66dc0a45d38d94221c60ecc5607e23e9bce2dee9
olivia <oliviawmackay@gmail.com>
2021-06-14 16:03:29 -0400

    mapping images to cat cards

--- src/page/Cats.js
+++ src/page/Cats.js
@@ -1,57 +1,29 @@
-import { Avatar, Grid, makeStyles, Paper, Typography } from "@material-ui/core";
+import { Card, CardMedia } from "@material-ui/core";
 import React from "react";
 
-const useStyles = makeStyles((theme) => ({
-  grid: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  text: {
-    marginLeft: "15px",
-    paddingTop: "100px",
-    fontSize: "10rem",
-  },
-  paper: {
-    display: "flex",
-    flexDirection: "row",
-    minWidth: "900px",
-    width: "300px",
-    paddingLeft: "-15px",
-    marginBottom: "15px",
-    background: theme.palette.secondary.main,
-    elevation: 0,
-  },
-}));
+function importAll(r) {
+  const images = {};
+  return r.keys().map((item) => {
+    console.log(`key item: ${item}`);
+    images[item.replace("./", "")] = r(item);
+  });
+}
+
+function CatCard(props) {
+  const image = props.image;
+  return (
+    <Card>
+      <CardMedia image={image} />
+    </Card>
+  );
+}
 
 export default function Cats() {
-  const classes = useStyles();
-
-  return (
-    <Grid container className={classes.grid}>
-      <Grid item>
-        <Paper className={classes.paper}>
-          <Avatar
-            alt="Scarf"
-            variant="rounded"
-            src={`${process.env.PUBLIC_URL}/cats/scarf.jpg`}
-          />
-          <Typography className={classes.text} variant="h5">
-            Scarf
-          </Typography>
-        </Paper>
-      </Grid>
-      <Grid item>
-        <Paper className={classes.paper}>
-          <Avatar
-            alt="Butter"
-            variant="rounded"
-            src={`${process.env.PUBLIC_URL}/cats/butter.jpg`}
-          />
-          <Typography className={classes.text} variant="h5">
-            Butter
-          </Typography>
-        </Paper>
-      </Grid>
-    </Grid>
+  const images = importAll(
+    require.context("./images/cats", false, /\.(png|jpg)$/)
   );
+
+  return images.map((image) => {
+    return <CatCard key={image} image={image} />;
+  });
 }
